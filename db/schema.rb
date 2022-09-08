0df6cd504ef9b97bbc30929c01822798cf2d5282
MarkBrianKariuki <kariukimarkbrian@gmail.com>
2022-09-08 10:35:21 +0300

    deployment to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_07_163010) do
-  create_table "drivers", force: :cascade do |t|
-    t.string "name"
-    t.string "licence_number"
-  end
-
-  create_table "garages", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "mechanic"
-    t.string "certification_number"
-  end
-
-  create_table "vehicles", force: :cascade do |t|
-    t.string "model"
-    t.string "manufacture_year"
-    t.string "last_service"
-    t.string "next_service"
-    t.string "catalytic_converter"
-    t.string "vehicle_insurance"
-    t.integer "driver_id"
-    t.integer "garage_id"
-    t.string "vehicle_registration"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
